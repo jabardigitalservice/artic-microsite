368dc9da7127e5498d72ede0a2883df8fa640860
Yoga Hanggara <yohang88@gmail.com>
2020-10-26 22:50:36 +0700

    form booking
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- pages/booking-form.vue
+++ pages/booking-form.vue
@@ -117,8 +117,8 @@
       <div class="mt-8 text-center">
         <button
           type="submit"
-          @click="submit"
           class="block w-full bg-blue-500 text-white font-semibold text-center py-2 px-4 rounded"
+          @click="submit"
         >
           Simpan
         </button>
@@ -137,7 +137,7 @@
 export default {
   methods: {
     submit() {
-      alert('Success')
+      this.$axios.post(`/register`)
     },
   },
 }
