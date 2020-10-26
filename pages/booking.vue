b2e461986280cb1c7143f99a5d482ad543c42f4c
Yoga Hanggara <yohang88@gmail.com>
2020-10-26 13:13:01 +0700

    booking form
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- pages/booking.vue
+++ pages/booking.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>Booking</div>
+  <div class="p-4">
+    <h2 class="text-xl text-gray-900 text-center">Syarat dan Ketentuan</h2>
+    <div class="mt-4 text-sm">
+      <p>
+        Pendaftaran Peserta Tes Masif COVID-19 bersifat terbatas dan
+        diprioritaskan bagi orang yang paling rentan terjangkit wabah virus
+        COVID-19.
+      </p>
+      <p class="mt-4">
+        Mendaftarkan diri tidak secara otomatis menjamin akses untuk melakukan
+        pengetesan.
+      </p>
+      <p class="mt-4">
+        Peserta akan melalui proses verifikasi dan validasi dari Dinas Kesehatan
+        Kota/Kabupaten dan Dinas Kesehatan Provinsi Jawa Barat, sebelum mendapat
+        undangan melakukan pengetesan.
+      </p>
+    </div>
+
+    <div class="mt-12 text-center">
+      <nuxt-link
+        to="/booking-form"
+        class="block bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white text-center py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        Ya, Saya Mengerti
+      </nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script>
